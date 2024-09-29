a4bfc285fe295baba7a8b74becde0b7848f78879
jffsly <jffslyyy@gmail.com>
2024-09-30 03:03:35 +0800

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <!-- CSS BOOTSTRAP LINK -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
-    <?php include('inc/navbar.html'); ?>
+    <?php include('inc/navbar.php'); ?>
 
 </head>
 
@@ -76,7 +76,7 @@
         </p>
     </div>
 
-    <?php include('inc/footer.html'); ?>
+    <?php include('inc/footer.php'); ?>
     <!-- JS BOOTSTRAP LINK -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
